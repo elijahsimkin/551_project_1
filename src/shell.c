67f4d1e9b329a13c0928c7f168d5c529c27fcd60
elijahawork <64611104+elijahawork@users.noreply.github.com>
2025-03-13 00:35:44 -0500

    add jobs

--- src/shell.c
+++ src/shell.c
@@ -7,8 +7,51 @@
 #include <fcntl.h>
 
 #define MAX_ARGS 128
+#define MAX_JOBS 64
 #define PIPE_READ 0
 #define PIPE_WRITE 1
+
+typedef struct {
+    int job_id;
+    pid_t pid;
+    char command[256];
+    int active;
+} Job;
+
+Job jobs[MAX_JOBS];
+int job_count = 0;
+
+void add_job(pid_t pid, char *command) {
+    if (job_count < MAX_JOBS) {
+        jobs[job_count].job_id = job_count + 1;
+        jobs[job_count].pid = pid;
+        strncpy(jobs[job_count].command, command, 255);
+        jobs[job_count].command[255] = '\0';
+        jobs[job_count].active = 1;
+        job_count++;
+    }
+}
+
+void check_jobs() {
+    for (int i = 0; i < job_count; i++) {
+        if (jobs[i].active) {
+            int status;
+            pid_t result = waitpid(jobs[i].pid, &status, WNOHANG);
+            if (result > 0) {
+                printf("[Job %d] Done: %s\n", jobs[i].job_id, jobs[i].command);
+                jobs[i].active = 0;
+            }
+        }
+    }
+}
+
+void list_jobs() {
+    for (int i = 0; i < job_count; i++) {
+        if (jobs[i].active) {
+            printf("[Job %d] Running: %s (PID: %d)\n", jobs[i].job_id, jobs[i].command, jobs[i].pid);
+        }
+    }
+}
 
 void execute_command(char *command, char **args, int input_fd, int output_fd, int background) {
     pid_t pid = fork();
@@ -24,13 +67,23 @@ void execute_command(char *command, char **args, int input_fd, int output_fd, in
         execvp(command, args);
         _exit(1);
     } else if (pid > 0) { // Parent process
-        if (!background) {
+        if (background) {
+            add_job(pid, command);
+            printf("[Job %d] Started: %s (PID: %d)\n", job_count, command, pid);
+        } else {
             waitpid(pid, NULL, 0);
         }
     }
 }
 
 void process_input(char *input) {
+    check_jobs();
+
+    if (strcmp(input, "jobs") == 0) {
+        list_jobs();
+        return;
+    }
+
     int pipe_fd[2], input_fd = 0;
     char *commands[MAX_ARGS];
     int command_count = 0;
