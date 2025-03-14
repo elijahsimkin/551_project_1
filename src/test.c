0fdfc8cd5d7e55b687577be0dbe990d608a49acb
sohinisahukar <sohini.sahukar@gmail.com>
2025-03-14 17:57:11 -0500

    boiler test code

---
+++ src/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    /* comment */
+    printf("Hello this is code from: windows vscode -> github -> ubuntu on docker -> minix on ubuntu on docker.\n");
+    return 0;
+}
